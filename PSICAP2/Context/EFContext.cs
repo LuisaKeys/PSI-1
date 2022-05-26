e6eea0ab01fd99b275cea8ee0e1fe80e472104c8
LuisaKeys <luisalima0602@gmail.com>
2022-05-26 10:26:12 -0300

    <Iniciando Fabricante>

---
+++ PSICAP2/Context/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSICAP2.Models;
+using System.Data.Entity;
+
+namespace PSICAP2.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
