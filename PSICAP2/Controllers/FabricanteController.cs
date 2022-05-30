d1357035f10ccf3cccd23fa4c7cf1b20c95aac78
LuisaKeys <luisalima0602@gmail.com>
2022-05-30 08:22:55 -0300

    <Implantando Banco de Dados>

---
+++ PSICAP2/Controllers/FabricanteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PSICAP2.Models;
+using PSICAP2.Context;
+
+namespace PSICAP2.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricante
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+    }
+}
--- PSICAP2/Models/Categoria.cs
+++ PSICAP2/Models/Categoria.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace PSICAP2.Models
 {
     public class Categoria
