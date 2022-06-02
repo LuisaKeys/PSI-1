f1de4e3fb3ecf98d8630a97b995b766aa1c7532a
LuisaKeys <luisalima0602@gmail.com>
2022-06-02 10:25:19 -0300

    <Continuando Fabricante sem banco de dados>

--- PSICAP2/Controllers/FabricanteController.cs
+++ PSICAP2/Controllers/FabricanteController.cs
@@ -8,13 +8,21 @@ using PSICAP2.Context;
 
 namespace PSICAP2.Controllers
 {
+   
     public class FabricanteController : Controller
     {
+        private static IList<Fabricante> fabricantes = new List<Fabricante>()
+       {
+       new Fabricante() { FabricanteId = 1, Nome = "LG"},
+       new Fabricante() { FabricanteId = 2, Nome = "Microsoft"}
+       };
         private EFContext context = new EFContext();
+
         // GET: Fabricante
         public ActionResult Index()
         {
-            return View(context.Fabricantes.OrderBy(c => c.Nome));
+            return View(fabricantes);
+            //return View(context.Fabricantes.OrderBy(c => c.Nome));
         }
     }
 }
