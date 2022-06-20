1ba2b9334fc8906fce8290cc947240277bf42f4d
LuisaKeys <luisalima0602@gmail.com>
2022-06-20 08:08:09 -0300

    <Fabricante finalizado com banco de dados>

--- PSICAP2/Controllers/FabricanteController.cs
+++ PSICAP2/Controllers/FabricanteController.cs
@@ -8,23 +8,17 @@ using PSICAP2.Context;
 using System.Net;
 using System.Data.Entity;
 
+
 namespace PSICAP2.Controllers
 {
    
     public class FabricanteController : Controller
     {
-        private static IList<Fabricante> fabricantes = new List<Fabricante>()
-       {
-       new Fabricante() { FabricanteId = 1, Nome = "LG"},
-       new Fabricante() { FabricanteId = 2, Nome = "Microsoft"}
-       };
         private EFContext context = new EFContext();
-
-        // GET: Fabricante
+        // GET: Fabricantes
         public ActionResult Index()
         {
-            return View(fabricantes);
-            //return View(context.Fabricantes.OrderBy(c => c.Nome));
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
         }
         // GET: Create
         public ActionResult Create()
@@ -36,10 +30,8 @@ namespace PSICAP2.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Fabricante fabricante)
         {
-            //context.Fabricantes.Add(fabricante);
-            //context.SaveChanges();
-            fabricantes.Add(fabricante);
-            fabricante.FabricanteId = fabricantes.Select(m => m.FabricanteId).Max() + 1;
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
         // GET: Fabricantes/Edit/5
@@ -49,8 +41,7 @@ namespace PSICAP2.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //Fabricante fabricante = context.Fabricantes.Find(id);
-            Fabricante fabricante = fabricantes.Where(m => m.FabricanteId == id).First();
+            Fabricante fabricante = context.Fabricantes.Find(id);
             if (fabricante == null)
             {
                 return HttpNotFound();
@@ -64,11 +55,8 @@ namespace PSICAP2.Controllers
         {
             if (ModelState.IsValid)
             {
-                //context.Entry(fabricante).State = EntityState.Modified;
-                //context.SaveChanges();
-                fabricantes.Remove(
-                fabricantes.Where(f => f.FabricanteId == fabricante.FabricanteId).First());
-                fabricantes.Add(fabricante);
+                context.Entry(fabricante).State = EntityState.Modified;
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(fabricante);
@@ -80,8 +68,7 @@ namespace PSICAP2.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //Fabricante fabricante = context.Fabricantes.Find(id);
-            Fabricante fabricante = fabricantes.Where(m => m.FabricanteId == id).First();
+            Fabricante fabricante = context.Fabricantes.Find(id);
             if (fabricante == null)
             {
                 return HttpNotFound();
@@ -95,15 +82,12 @@ namespace PSICAP2.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //Fabricante fabricante = context.Fabricantes.Find(id);
-            Fabricante fabricante = fabricantes.Where(m => m.FabricanteId == id).First();
-            fabricantes.Remove(fabricante);
-            return RedirectToAction("Index");
-            //if (fabricante == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            //return View(fabricante);
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
         }
         // POST: Fabricantes/Delete/5
         [HttpPost]
