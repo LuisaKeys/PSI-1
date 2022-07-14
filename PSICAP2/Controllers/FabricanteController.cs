088021feaaa9b3dc71f96e2604a3d7441b9ffe80
LuisaKeys <luisalima0602@gmail.com>
2022-07-14 10:16:15 -0300

    <PASSAR VISÕES PAA REPOSITORIO REMOTO>

--- PSICAP2/Controllers/FabricanteController.cs
+++ PSICAP2/Controllers/FabricanteController.cs
@@ -97,6 +97,7 @@ namespace PSICAP2.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
     }
