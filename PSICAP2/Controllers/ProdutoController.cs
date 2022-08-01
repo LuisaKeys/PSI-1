a927be8cba93da8aebbc0479ffd10175b7a0893c
LuisaKeys <luisalima0602@gmail.com>
2022-08-01 08:25:39 -0300

    <Atualizando Produto>

--- PSICAP2/Controllers/ProdutoController.cs
+++ PSICAP2/Controllers/ProdutoController.cs
@@ -5,16 +5,19 @@ using System.Web;
 using System.Web.Mvc;
 using PSICAP2.Context;
 using PSICAP2.Models;
+using System.Data.Entity;
 
 namespace PSICAP2.Controllers
 {
     public class ProdutoController : Controller
     {
         private EFContext context = new EFContext();
-        // GET: Produtos
+        //GET: Produtos
         public ActionResult Index()
         {
-            return View(context.Produtos.OrderBy(c => c.Nome));
+            var produtos = context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).
+            OrderBy(n => n.Nome);
+            return View(produtos);
         }
 
         // GET: Produto/Details/5
@@ -23,25 +26,30 @@ namespace PSICAP2.Controllers
             return View();
         }
 
-        // GET: Produto/Create
+        // GET: Produtos/Create
         public ActionResult Create()
         {
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome),
+            "CategoriaId", "Nome");
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome),
+            "FabricanteId", "Nome");
             return View();
         }
 
-        // POST: Produto/Create
+        // POST: Produtos/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Produto produto)
         {
             try
             {
                 // TODO: Add insert logic here
-
+                context.Produtos.Add(produto);
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(produto);
             }
         }
 
