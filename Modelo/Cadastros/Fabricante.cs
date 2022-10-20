df4c383033be3c251a688f30f6513d7691e2958a
Luisa Weydner De Lima Chaves <luisa.lima@academico.ifrn.edu.br>
2022-10-20 11:40:47 -0300

    <ATIVIDADE - TERCEIRO BIMESTRE>

--- Modelo/Cadastros/Fabricante.cs
+++ Modelo/Cadastros/Fabricante.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace Modelo.Cadastros
 {
     public class Fabricante
     {
+        [DisplayName("Fabricante")]
         public long FabricanteId { get; set; }
         public string Nome { get; set; }
         public virtual ICollection<Produto> Produtos { get; set; }
--- Modelo/Tabelas/Categoria.cs
+++ Modelo/Tabelas/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using Modelo.Cadastros;
@@ -9,6 +10,7 @@ namespace Modelo.Tabelas
 {
     public class Categoria
     {
+        [DisplayName("Categoria")]
         public long CategoriaId { get; set; }
         public string Nome { get; set; }
         public virtual ICollection<Produto> Produtos { get; set; }
