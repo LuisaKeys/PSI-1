4af49b006057a82412095804f801a08853a8ff68
Luísa Lima <luisalima0602@gmail.com>
2022-12-10 13:05:10 -0300

    <ATIVIDADE PT.1>

---
+++ PSICAP2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Servico.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Servico.Cadastros;
+
+namespace PSICAP2.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProdutoServico produtoServico = new ProdutoServico();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(produtoServico.ObterProdutosClassificadosPorNome());
+        }
+    }
+}
